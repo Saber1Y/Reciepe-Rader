5addca6a3c0fc5b1f78dc7480f3390f20a25e5e6
ayodeji <ayodejiakintobi1@gmail.com>
2023-09-04 10:24:58 +0100

    Responsive completed

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
 const menuIcon = document.getElementById('menu-icon');
 const navLinks = document.getElementById('nav-links');
+const bar1 = document.getElementById('bar1');
+const bar2 = document.getElementById('bar2');
+const bar3 = document.getElementById('bar3');
 
 menuIcon.addEventListener('click', function() {
-    navLinks.classList.toggle('show-nav');
+  menuIcon.classList.toggle('is-active');
+  navLinks.classList.toggle('show-nav');
+
+  bar1.classList.toggle('rotate-bar1');
+  bar2.classList.toggle('hide-bar2');
+  bar3.classList.toggle('rotate-bar3');
+
+ 
+  if (navLinks.classList.contains('show-nav')) {
+    document.body.style.overflow = 'hidden';
+  } else {
+    document.body.style.overflow = '';
+  }
 });
+
+
 
 document.addEventListener("DOMContentLoaded", function () {
     const modalLinks = document.querySelectorAll(".modal-link");
